2ccfb3d0776ca764e0c725f58099c2a204af984a
Dušan Grmela <dusan.grmela@idc.cz>
2018-02-19 22:22:20 +0100

    fix STDOUT

--- Libs/Log.pm
+++ Libs/Log.pm
@@ -152,10 +152,13 @@ sub write {
     my $time   = scalar localtime(time);
 
     # PRINT webout.log
-    open($output, '>>', $logout) or die "Error Open log:'$logout'";
-    print $output "[$time] $ip $text\n" or die "Error Write log:'$logout'";
-    print STDOUT "PRINT LOG: $text\n" if ($self->getLogLevel() > 4 and $self->filterLog('print', $who));
-    close($output) or die "Error Close log:'$logout'";
+    if ($self->getLogLevel() > 4 and $self->filterLog('print', $who)) {
+	print STDOUT "PRINT LOG: $text\n";
+    } else {
+	open($output, '>>', $logout) or die "Error Open log:'$logout'";
+	print $output "[$time] $ip $text\n" or die "Error Write log:'$logout'";
+	close($output) or die "Error Close log:'$logout'";
+    }
 
     # ZAMCENI
     #flock($output,2) or die "Error Lock log:'$logout'";
